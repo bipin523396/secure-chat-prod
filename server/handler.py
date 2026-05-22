bbcf2daa3ff692b69304282443c79ede5a96b8cc
Bipin Yadav <bipin@example.com>
2026-05-23 03:54:29 +0530

    Major architectural change: Move API to server/ folder to force cache bypass

--- server/handler.py
+++ server/handler.py
@@ -3,7 +3,7 @@ import sys
 import traceback
 from flask import Flask, jsonify
 
-# Ensure the 'api' directory is in the path so 'app' can be imported
+# Ensure the 'server' directory is in the path so 'app' can be imported
 sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
 
